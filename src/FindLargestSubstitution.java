b98f5c50af22dc9710a79f3b53b6c78e77a0c68f
hareshaprajapati <hareshaprajapati@gmail.com>
2018-05-24 22:04:06 +0530

    Minor change.

--- src/FindLargestSubstitution.java
+++ src/FindLargestSubstitution.java
@@ -1,7 +1,7 @@
 public class FindLargestSubstitution {
 
     public static void main(String[] args) {
-        String str = "abcdbcbcbcbbcabcbcccccb";
+        String str = "wwqqqqqwaaaacaac";
         System.out.println(findLongestSubstitution(str));
     }
 
@@ -10,23 +10,45 @@ public class FindLargestSubstitution {
         char[] ch = str.toCharArray();
         int length = str.length();
         String currentMax = "";
+        char ch1 = 0;
+        char ch2 = 0;
+        int ch2Count =0;
+        if(str.length()<=2){
+            return str;
+        }
         for(int i =0; i < length; i++){
-            if(i+1 < length && i+2 < length){
-                if(ch[i] == ch[i+1] || ch[i] == ch[i+2] || sb.toString().contains(Character.toString(ch[i]))){
-                    sb.append(ch[i]);
-                }else{
-                    currentMax = sb.toString();
-                    sb.delete(0,sb.length());
-                }
-            }/*else if(i+1 < length){
-                if(ch[i] == ch[i+1] || sb.toString().contains(Character.toString(ch[i]))){
-                    sb.append(ch[i]);
-                }
-            }*/else{
-                if(sb.toString().contains(Character.toString(ch[i]))){
-                    sb.append(ch[i]);
-                }
+            if(ch1 == 0 || ( ch1 == ch[i] && ch2 == 0 )){
+                sb.append(ch[i]);
+                ch1 = ch[i];
+                continue;
             }
+            if(ch2==0 && ch1 != 0){
+                ch2 = ch[i];
+                sb.append(ch[i]);
+                ch2Count++;
+                continue;
+            }
+            if(ch1 == ch[i]){
+                sb.append(ch[i]);
+                ch1 = ch2;
+                ch2 = ch[i];
+                ch2Count=1;
+            }else if(ch2 == ch[i]){
+                ch2Count++;
+                sb.append(ch[i]);
+            }else if(ch1 != ch[i] && ch2 != ch[i]){
+                ch1 = ch2;
+                ch2 = ch[i];
+                String substring = sb.substring(sb.length() - ch2Count);
+                ch2Count = 1;
+                if(currentMax.length() < sb.length()){
+                    currentMax = sb.toString();
+                }
+                sb.delete(0,sb.length());
+                sb.append(substring);
+                sb.append(ch2);
+            }
+
         }
         if(currentMax.length() == sb.toString().length()){
             return currentMax + "  " + sb.toString();
