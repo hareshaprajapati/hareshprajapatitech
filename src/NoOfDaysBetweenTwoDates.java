05c4cd47df2a86790e2621801224775d8cd5643b
hareshp-ezdi <haresh.p@ezdi.us>
2018-05-25 16:59:12 +0530

    minor change

--- src/NoOfDaysBetweenTwoDates.java
+++ src/NoOfDaysBetweenTwoDates.java
@@ -34,60 +34,31 @@ public class NoOfDaysBetweenTwoDates {
         this.year = year;
     }
 
-    public NoOfDaysBetweenTwoDates addOrSubDays(int amount) {
-        this.amount = amount;
-        return addOrSubDays(this.day + this.amount, this.month, this.year);
-    }
-
-
-    public static int countLeapYears(NoOfDaysBetweenTwoDates d) {
-
-        int years = d.year;
-
-
-        // Check if the current year needs to be considered
-        // for the count of leap years or not
-        if (d.month < 2)
-            years--;
-
-
-
-        // An year is a leap year if it is a multiple of 4,
-        // multiple of 400 and not a multiple of 100.
-        return years / 4 - years / 100 + years / 400;
-    }
-
-    public static long getDifference(NoOfDaysBetweenTwoDates dt1, NoOfDaysBetweenTwoDates dt2) {
-        // COUNT TOTAL NUMBER OF DAYS BEFORE FIRST DATE 'dt1'
-
-        // initialize count using years and day
-        long n1 = (dt1.year -1) * 365 + dt1.day;
-
-        // Add days for months in given date
-        for (int i = 1; i <= dt1.month ; i++)
-            n1 += daysInMonth.get(i);
-
-        // Since every leap year is of 366 days,
-        // Add a day for every leap year
-        n1 += countLeapYears(dt1);
-
-        // SIMILARLY, COUNT TOTAL NUMBER OF DAYS BEFORE 'dt2'
-
-    long n2 = (dt2.year - 1) * 365 + dt2.day;
-        for (int i = 1; i <= dt2.month ; i++)
-            n2 += daysInMonth.get(i);
-        n2 += countLeapYears(dt2);
-
-        // return difference between two counts
-        return (n2 - n1);
-    }
-
     public static void main(String[] args) {
         int amount = 0;
         amount = -15;
 //        amount = 21;
+        int day1= 28 , day2= 25;
+        int m1 = 4, m2 = 1;
+        int y1= 2000, y2=2018;
 
-        NoOfDaysBetweenTwoDates addSubtractDate = new NoOfDaysBetweenTwoDates(8, 1, 2018);
+        NoOfDaysBetweenTwoDates addSubtractDate = new NoOfDaysBetweenTwoDates(day1, m1, y1);
+        NoOfDaysBetweenTwoDates myDate1 = new NoOfDaysBetweenTwoDates(day2, m2, y2);
+        System.out.println(getDifference(addSubtractDate, myDate1));
+
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.MONTH,m1-1);
+        cal.set(Calendar.DAY_OF_MONTH,day1);
+        cal.set(Calendar.YEAR,y1);
+        long timeInMillis = cal.getTimeInMillis();
+        cal.set(Calendar.MONTH,m2-1);
+        cal.set(Calendar.DAY_OF_MONTH,day2);
+        cal.set(Calendar.YEAR,y2);
+        long timeInMillis1 = cal.getTimeInMillis();
+        long diff = timeInMillis1 - timeInMillis;
+        System.out.println(diff / (24 * 60 * 60 * 1000));
+
+        /*NoOfDaysBetweenTwoDates addSubtractDate = new NoOfDaysBetweenTwoDates(8, 1, 2018);
         NoOfDaysBetweenTwoDates myDate1 = new NoOfDaysBetweenTwoDates(13, 2, 2018);
 
         System.out.println(getDifference(addSubtractDate, myDate1));
@@ -100,78 +71,67 @@ public class NoOfDaysBetweenTwoDates {
         cal.set(Calendar.DAY_OF_MONTH,13);
         long timeInMillis1 = cal.getTimeInMillis();
         long diff = timeInMillis1 - timeInMillis;
-        System.out.println(diff / (24 * 60 * 60 * 1000));
+        System.out.println(diff / (24 * 60 * 60 * 1000));*/
     }
-
-    private NoOfDaysBetweenTwoDates addOrSubDays(int days, int month, int year) {
-        if (days > 0 && days <= getNoOfDaysInMonth(month, year)) {
-            return new NoOfDaysBetweenTwoDates(days, month, year);
-        } else if (days <= 0) {
-            month = month - 1;
-            if (month == 0) {
-                month = 12;
-                year = year - 1;
+    public static long getDifference(NoOfDaysBetweenTwoDates dt1, NoOfDaysBetweenTwoDates dt2) {
+        // COUNT TOTAL NUMBER OF DAYS BEFORE FIRST DATE 'dt1'
+        long day =0;
+            int y1 = dt1.year;
+            int y2 = dt2.year;
+            int m1 = dt1.month;
+            int m2 = dt2.month;
+            int d1 = dt1.day;
+            int d2 = dt2.day;
+            boolean currentMonthCalculated = false;
+            if(y1 > y2){
+                int temp = y1;
+                y1=y2;
+                y2=temp;
+                temp = m1;
+                m1 = m2;
+                m2 =temp;
+                temp = d1;
+                d1 = d2;
+                d2 = temp;
             }
-            days = days + getNoOfDaysInMonth(month, year);
-        } else {
-            days = days - getNoOfDaysInMonth(month, year);
-            month = month + 1;
-            if (month > 12) {
-                month = 1;
-                year = year + 1;
+            if(y1 <= y2) {
+                /*int yearDiff = y2 - y1-1;
+                for(int i=y1;i<y2;i++){
+                    if(checkIsLeepYear(i)){
+                        day++;
+                    }
+                }
+                day+= yearDiff*365;
+                y1=y2;*/
+                    while (m1 <= 12 && y1 <= y2 ) {
+                        System.out.println("calculating day for the month " + m1 + " and year "  + y1);
+                        if (m1 == dt1.month && !currentMonthCalculated) {
+                            if(checkIsLeepYear(y1) && m1 ==2) day++;
+                            day += daysInMonth.get(m1) - d1;
+                            currentMonthCalculated = true;
+                        }else{
+                            if(y1 == y2 && m1 == m2){
+                                day+= d2;
+                                break;
+                            }else{
+                                if(checkIsLeepYear(y1) && m1 ==2) day++;
+                                day+= daysInMonth.get(m1);
+                            }
+                        }
+                        m1++;
+                        if(m1 == 13){
+                            y1++;
+                            m1=1;
+                        }
+                    }
             }
-
-        }
-        return addOrSubDays(days, month, year);
+        return day;
     }
 
-    private int getNoOfDaysInMonth(int month, int year) {
-        if (month == 2 && checkIsLeepYear(year)) {
-            return daysInMonth.get(month) + 1;
-        }
-        return daysInMonth.get(month);
-    }
-
-    private boolean checkIsLeepYear(int year) {
+    private static boolean checkIsLeepYear(int year) {
         if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))) {
             return true;
         }
         return false;
-    }
-
-    public int getDay() {
-        return day;
-    }
-
-    public int getMonth() {
-        return month;
-    }
-
-    public int getYear() {
-        return year;
-    }
-
-    public int getAmount() {
-        return amount;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof NoOfDaysBetweenTwoDates)) return false;
-
-        NoOfDaysBetweenTwoDates that = (NoOfDaysBetweenTwoDates) o;
-
-        if (day != that.day) return false;
-        if (month != that.month) return false;
-        return year == that.year;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = day;
-        result = 31 * result + month;
-        result = 31 * result + year;
-        return result;
     }
 }
